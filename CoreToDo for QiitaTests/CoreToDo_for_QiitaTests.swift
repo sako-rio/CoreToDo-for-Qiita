2cb7a317615bb13a4dad37603df1721bc104a15d
sako <sako@rio.ne.jp>
2017-09-01 14:03:42 +0900

    Initial Commit

---
+++ CoreToDo for QiitaTests/CoreToDo_for_QiitaTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreToDo_for_QiitaTests.swift
+//  CoreToDo for QiitaTests
+//
+//  Created by sako kazuhiro on 2017/09/01.
+//  Copyright © 2017年 sako kazuhiro. All rights reserved.
+//
+
+import XCTest
+@testable import CoreToDo_for_Qiita
+
+class CoreToDo_for_QiitaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
